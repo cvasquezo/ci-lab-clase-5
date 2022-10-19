21370e65ca7ca54fd81a70f94540ac303bf33e1c
cvasquezo <115799289+cvasquezo@users.noreply.github.com>
2022-10-19 13:02:30 -0300

    Update AppTest.java
    jsjsjs 13:02

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -11,11 +11,10 @@ import org.junit.Test;
 public class AppTest {
   @Test
   public void probarTriangulo() {   
-    assertEquals("es un triangolo pitagorico", App.triangulo(3, 4, 5));
+    assertEquals("error no es un triangulo", App.triangulo(0, 0, 0));
     assertEquals("es un triangulo equilatero", App.triangulo(1, 1, 1));
     assertEquals("es un triangulo isoceles", App.triangulo(2, 2, 3));
     assertEquals("es un triangolo escaleno", App.triangulo(3, 4, 6));
-    assertEquals("error no es un triangulo", App.triangulo(0, 0, 0));
-    
+    assertEquals("es un triangolo pitagorico", App.triangulo(3, 4, 5));
   }
 }
