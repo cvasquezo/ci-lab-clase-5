c99ac96abbccfc8faa55818ff092caa7fa49be9f
cvasquezo <115799289+cvasquezo@users.noreply.github.com>
2022-10-19 11:16:31 -0300

    Update App.java
    modificado 11:16

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,11 +7,13 @@ package com.mycompany.app;
 public class App {
   public static String triangulo(int a, int b, int c) {
 
-  if ((a*a)+(b*b) == (c*c)) {
-    return "es un triangulo pitagorico";
-  }
+
     if (!(a + b > c && a + c > b && b + c > a)) {
       return "error no es un triangulo";
+    }
+    
+    if ((a*a)+(b*b) == (c*c)) {
+    return "es un triangulo pitagorico";
     }
     if (a == b && a == c && b == c) {
       return "es un triangulo equilatero";
